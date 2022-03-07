e7429880e16a51ceb3dd37ebf2a72da981feb739
Ahamed <ahamedali95@gmail.com>
2022-03-06 19:59:44 -0500

    removed unused dependencies

--- src/index.tsx
+++ src/index.tsx
@@ -4,9 +4,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import {ThemeProvider} from '@material-ui/core/styles';
-import { MuiPickersUtilsProvider } from '@material-ui/pickers';
-
-import DateFnsUtils from '@date-io/date-fns';
 
 import * as serviceWorker from './service-worker';
 import AppContainer1 from './components/AppContainer';
@@ -16,9 +13,7 @@ import './styles/index.scss';
 
 ReactDOM.render(
     <ThemeProvider theme={theme}>
-        <MuiPickersUtilsProvider utils={DateFnsUtils}>
-            <AppContainer1 />
-        </MuiPickersUtilsProvider>
+        <AppContainer1 />
     </ThemeProvider>,
     document.getElementById('root'));
 
